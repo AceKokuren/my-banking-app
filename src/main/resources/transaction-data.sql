a4de2cb0b94b028d7548329757c33d67c15ae8ae
Cameron Black <cameronablack1998@gmail.com>
2024-10-28 17:01:29 +0000

    Added more test data from coding exercise instructions.

--- src/main/resources/transaction-data.sql
+++ src/main/resources/transaction-data.sql
@@ -1,3 +1,7 @@
-INSERT INTO transaction (transaction_date, transaction_vendor, transaction_type, transaction_amount,
+INSERT INTO `transaction` (transaction_date, transaction_vendor, transaction_type, transaction_amount,
                          transaction_category)
-VALUES ('2020-11-01', 'Morrisons', 'card', '10.40', 'Groceries');
+VALUES ('2020-11-01', 'Morrisons', 'card', '10.40', 'Groceries'),
+       ('2020-10-28', 'CYBG', 'direct debit', '600.00', 'MyMonthlyDD'),
+       ('2020-10-28', 'PureGym', 'direct debit', '40.00', 'MyMonthlyDD'),
+       ('2020-10-01', 'M&S', 'card', '5.99', 'Groceries'),
+       ('2020-09-30', 'McMillan', 'internet', '10.00', '');
